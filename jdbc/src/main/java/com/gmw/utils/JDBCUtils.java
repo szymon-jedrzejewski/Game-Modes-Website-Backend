f95bbaf5036ec19377db48d4571e2b00b332b0de
Szymon Jedrzejewski <szymonj219@gmail.com>
2022-12-24 08:11:57 +0100

    Add hikarcicp for pool management.

--- jdbc/src/main/java/com/gmw/utils/JDBCUtils.java
+++ jdbc/src/main/java/com/gmw/utils/JDBCUtils.java
@@ -3,18 +3,32 @@ package com.gmw.utils;
 
 import com.gmw.reader.JsonConfigReader;
 import com.gmw.reader.tos.DatabaseConfig;
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
 
 import java.sql.Connection;
-import java.sql.DriverManager;
 import java.sql.SQLException;
 
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class JDBCUtils {
 
-    private JDBCUtils() {
+    private static final HikariConfig config = new HikariConfig();
+    private static final HikariDataSource ds;
+
+    static {
+        DatabaseConfig databaseConfig = JsonConfigReader.readDatabaseConfig();
+        config.setJdbcUrl(databaseConfig.url());
+        config.setUsername(databaseConfig.user());
+        config.setPassword(databaseConfig.password());
+        config.addDataSourceProperty("cachePrepStmts", "true");
+        config.addDataSourceProperty("prepStmtCacheSize", "250");
+        config.addDataSourceProperty("prepStmtCacheSqlLimit", "2048");
+        ds = new HikariDataSource(config);
     }
 
     public static Connection getConnection() throws SQLException {
-        DatabaseConfig databaseConfig = JsonConfigReader.readDatabaseConfig();
-        return DriverManager.getConnection(databaseConfig.url(), databaseConfig.user(), databaseConfig.password());
+        return ds.getConnection();
     }
 }
