cd83ae9b9379356fd5c8133cd786af04027d87c8
Szymon <szymonj219@gmail.com>
2023-02-22 21:53:50 +0100

    Remove unnecessary annotation

--- api/src/main/java/com/gmw/api/rest/activity/user/CreateUserActivity.java
+++ api/src/main/java/com/gmw/api/rest/activity/user/CreateUserActivity.java
@@ -10,9 +10,7 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.stereotype.Component;
 
-@Component
 @AllArgsConstructor
 public class CreateUserActivity extends Activity<Void> {
 
